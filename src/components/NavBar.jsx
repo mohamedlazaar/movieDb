e078b36f5755002820e827aeff4386e2d17e617e
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-10-04 08:30:39 +0100

    final organization

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom'
 function NavBar() {
   return (
     <div className='container justify-between w-full flex flex-row '>
-        <h1 className='text-white text-xl '>MovieDB</h1>
+      {/* title */}
+        <Link to='/'><h1 className='text-white text-xl '>MovieDB</h1></Link>
+        {/* links */}
         <div className=" flex flex-row gap-5">
+          {/* Search Link */}
         <Link to='/' className='text-white hover:text-black transition-all'>Search</Link>
         <Link to='/favorites' className='text-white hover:text-black transition-all'>Favorites</Link>
         </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function SearchBar(props) {
   return (
     <div className='flex flex-col w-full justify-center gap-10 items-center h-96'>
+      {/* search title */}
         <h1 className='text-3xl md:text-5xl font-bold text-white'>Search Your Movie</h1>
+        {/* search Input */}
       <input 
         value={props.searchMovie} 
         onChange={(event) => props.setSearch(event.target.value)} 
