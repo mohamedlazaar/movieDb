c9df90b9bb1af3a760cd5626399a5971faa6c4e1
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-10-04 09:25:29 +0100

    ...

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,8 +9,8 @@ function NavBar() {
         {/* links */}
         <div className=" flex flex-row gap-5">
           {/* Search Link */}
-        <Link to='/' className='text-white hover:text-black transition-all'>Search</Link>
-        <Link to='/favorites' className='text-white hover:text-black transition-all'>Favorites</Link>
+        <Link to='/' className='text-white hover:text-black transition-all underline'>Search</Link>
+        <Link to='/favorites' className='text-white hover:text-black transition-all underline'>Favorites</Link>
         </div>
    
     </div>
