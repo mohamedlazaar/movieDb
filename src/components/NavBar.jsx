824546e18c4cde13022a5fe5227db9cb5c83b833
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-10-04 09:27:25 +0100

    changing some colors

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,12 +5,12 @@ function NavBar() {
   return (
     <div className='container justify-between w-full flex flex-row '>
       {/* title */}
-        <Link to='/'><h1 className='text-white text-xl '>MovieDB</h1></Link>
+        <Link to='/'><h1 className='text-black text-xl '>MovieDB</h1></Link>
         {/* links */}
         <div className=" flex flex-row gap-5">
           {/* Search Link */}
-        <Link to='/' className='text-white hover:text-black transition-all underline'>Search</Link>
-        <Link to='/favorites' className='text-white hover:text-black transition-all underline'>Favorites</Link>
+        <Link to='/' className='text-black hover:text-white transition-all underline'>Search</Link>
+        <Link to='/favorites' className='text-white hover:text-white transition-all underline'>Favorites</Link>
         </div>
    
     </div>
